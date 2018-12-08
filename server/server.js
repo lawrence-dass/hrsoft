6855d2a5a1a95c5e0d176278e2aa06fba78b06a5
Lawrence <Lawrence4code@gmail.com>
2018-12-08 06:26:48 +0530

    Create app in heroku and setup heroku CLI

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-
 const app = express();
 const path = require('path');
-
 const publicPath = path.join(__dirname, '..', '/prod');
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('port running at 3000');
 });
