6fb7ffa2634b98aabf986c6f8bc0ced44e41e788
Lawrence <Lawrence4code@gmail.com>
2018-12-14 03:28:13 +0530

    API key issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,6 @@
 import * as firebase from 'firebase';
 
-const config = {
-  apiKey: 'AIzaSyCzIb7N7F-8gRSg_HwmPOHN5_P0Zlo5vKI',
-  authDomain: 'hrsoft-65876.firebaseapp.com',
-  databaseURL: 'https://hrsoft-65876.firebaseio.com',
-  projectId: 'hrsoft-65876',
-  storageBucket: 'hrsoft-65876.appspot.com',
-  messagingSenderId: '799183706016'
-};
+const config = {};
 
 firebase.initializeApp(config);
 
