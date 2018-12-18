ef223dbebf51eeebc6ea7a4bf6170dee7491f36c
Lawrence <Lawrence4code@gmail.com>
2018-12-18 06:54:05 +0530

    Added private routes for app access

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Header from '../components/Header';
+
 import LoginPage from '../components/LoginPage';
 import ClientDashboardPage from '../components/ClientDashboardPage';
 import AddClientPage from '../components/AddClientPage';
@@ -9,19 +9,19 @@ import EditClientPage from '../components/EditClientPage';
 import ViewClientPage from '../components/ViewClientPage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact />
-        <Route path="/dashboard" component={ClientDashboardPage} exact />
-        <Route path="/create" component={AddClientPage} />
-        <Route path="/edit/:id" component={EditClientPage} />
-        <Route path="/view/:id" component={ViewClientPage} />
+        <PrivateRoute path="/dashboard" component={ClientDashboardPage} exact />
+        <PrivateRoute path="/create" component={AddClientPage} />
+        <PrivateRoute path="/edit/:id" component={EditClientPage} />
+        <PrivateRoute path="/view/:id" component={ViewClientPage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
